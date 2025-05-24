4b98eb8dc84af465492988b662f84fda412382c3
pandiji <skm808478@gmail.com>
2025-05-25 01:39:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// backend/server.js
+require('dotenv').config();
+const express = require('express');
+const twilio = require('twilio');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
+
+app.post('/send', async (req, res) => {
+  const {  message,customers } = req.body;
+  console.log(customers[0].phone,message);
+  try {
+    const msg = await client.messages.create({
+      from: 'whatsapp:+14155238886',
+      to: `whatsapp:${customers[0].phone}`,
+      body: message,
+    });
+    res.status(200).json({ success: true, sid: msg.sid });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => console.log('Backend running on port 3000'));
